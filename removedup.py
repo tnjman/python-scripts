3cf5b26345e012066c7d5034f02021d8b49d4f4b
Jeff Mason <85841932+tnjman@users.noreply.github.com>
2021-06-13 15:43:17 -0500

    removedup.py
    Rev #2 - added more logic; non-empty "lista2"; if, else logic; and some useful comments, plus much nicer output from the print/display statements

--- removedup.py
+++ removedup.py
@@ -1,9 +1,21 @@
-# Orig by "redrambles:" https://gist.github.com/redrambles/c099b771a76e7ade81b5fb463b72126b
+# Orig: https://gist.github.com/redrambles/c099b771a76e7ade81b5fb463b72126b
+# Improved below:
+
 def remove_duplicates(lista):
-    lista2 = []
+# -- Start of function
+#   lista2 = [] <-- Orig had an empty "lista2"
+    lista2 = [2,4,6] # <- Pre-populate lista2 for different behavior
     for item in lista:
         if item not in lista2: #is item in lista2 already?
             lista2.append(item)
+        else: 
+            print ("Item ", item, " already in lista2; item not added.")
     return lista2
+# -- End of function
+print("Here is the list, with unique values: ", remove_duplicates([1,2,3,3])) 
 
-print(remove_duplicates([1,2,3,3]))  
+# Sample output:
+# C:\scripts>py removedup.py
+# Item  2  already in lista2; item not added.
+# Item  3  already in lista2; item not added.
+# Here is the list, with unique values: [2, 4, 6, 1, 3]
